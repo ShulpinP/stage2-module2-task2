0423378695ebb6010988820ca2e37b621c27d29e
ShulpinP <96373490+ShulpinP@users.noreply.github.com>
2022-12-04 08:59:11 +0300

    1

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,41 @@
 package com.example.servlet;
 
-public class LoginServlet {
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    private final Users users = Users.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user") != null) {
+            req.getRequestDispatcher("/user/hello.jsp").forward(req,resp);
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req,resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if ((Arrays.asList(users).contains(req.getAttribute("login")))
+                && req.getAttribute("password") != ("")) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", req.getAttribute("login"));
+            req.getRequestDispatcher("/user/hello.jsp");
+        } else {
+            req.getRequestDispatcher("/login.jsp");
+        }
+    }
+
     //write your code here!
 }
